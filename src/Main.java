7b3c7055b2f5a7c5235889e03f434fa0e63f80bc
Olga Golubkova <Golubkova23@gmail.com>
2022-06-12 16:09:29 +0300

    Innitial commit

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+
+        Set<Integer> positiveEvenIntList = new TreeSet<>();
+
+        for (int number : intList) {
+            if (number > 0 && number % 2 == 0) {
+                positiveEvenIntList.add(number);
+            }
+        }
+
+        for (int positiveEvenNumber : positiveEvenIntList) {
+            System.out.println(positiveEvenNumber);
+        }
+
+    }
+}
+
+
+
